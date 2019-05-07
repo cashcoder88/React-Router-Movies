4abce1a92f80faa33263c6410bb776f99167acce
cashcoder88 <cashglobe@gmail.com>
2019-05-07 15:24:48 -0600

    Finished MVP

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -35,21 +35,23 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <NavLink exact to={`/movies/${movie.id}`}> 
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+    <div className="movie-card">
+      <NavLink to={`/movies/${movie.id}`}> 
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
         </div>
-      ))}
-    </NavLink>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </NavLink>
+    </div>
   );
 }
