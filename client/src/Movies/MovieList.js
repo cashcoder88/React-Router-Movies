93b24bee82aa3b037c22afdc1a64bfac60ef7db9
cashcoder88 <cashglobe@gmail.com>
2019-05-07 15:29:40 -0600

    Looking to do first stretch

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -36,22 +36,22 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
-      <NavLink to={`/movies/${movie.id}`}> 
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
+    <NavLink to={`/movies/${movie.id}`}> 
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </NavLink>
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </NavLink>
     </div>
   );
 }
